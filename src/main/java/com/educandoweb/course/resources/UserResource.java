8197fe4cab290d96ea8855b06a29b63f7c79d197
Heitor Martini <martiniheitor@gmail.com>
2024-10-18 11:13:53 -0300

    User update

--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -16,6 +16,8 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.educandoweb.course.entities.User;
 import com.educandoweb.course.services.UserService;
+import org.springframework.web.bind.annotation.PutMapping;
+
 
 
 @RestController
@@ -50,5 +52,10 @@ public class UserResource {
 		return ResponseEntity.noContent().build();
 	}
 	
-
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User obj) {
+		obj = service.update(id, obj);
+		return ResponseEntity.ok().body(obj);
+	}
+	
 }
